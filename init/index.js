1c40344e2309a3488a1fd39e80c0ef613db836bc
sumitKumar92-webD <kumarsumit62436@gmail.com>
2026-07-26 15:32:57 +0530

    fix MongoAtlas Url

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wenderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wenderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(() =>{
     console.log("connected to DB");  
@@ -20,7 +21,7 @@ const initDb = async () =>{
    initData.data = initData.data.map((obj) => ({
     ...obj,
      owner: new
-     mongoose.Types.ObjectId ("69d3f036c8e191eec984c875"),
+     mongoose.Types.ObjectId ("6a62436c3d8057c14bdbe4d6"),
    }));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
